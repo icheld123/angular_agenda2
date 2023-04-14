fcdea2de5d3ff3ffb141040f361929851398c88c
Natalia Barbosa <48846184+natisbar@users.noreply.github.com>
2023-04-14 10:51:34 -0500

    Ajuste en footer y visual de telefono

--- src/app/feature/inicio/inicio.module.ts
+++ src/app/feature/inicio/inicio.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { IgxCarouselModule, IgxSliderModule } from "igniteui-angular";
 import { NgxPaginationModule } from "ngx-pagination";
-import { EquipoComponentComponent } from "./components/equipo-component/equipo-component.component";
 import { InicioComponentComponent } from "./components/inicio-component/inicio-component.component";
 import { ModalComponentComponent } from "./components/modal-component/modal-component.component";
 import { SliderComponentComponent } from "./components/slider-component/slider-component.component";
@@ -12,7 +11,6 @@ import { SliderComponentComponent } from "./components/slider-component/slider-c
   declarations: [
     InicioComponentComponent,
     ModalComponentComponent,
-    EquipoComponentComponent,
     SliderComponentComponent
   ],
   imports: [
